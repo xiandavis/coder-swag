6f1625d71fd9485266e95639a37443dbe9e9a449
xiandavis <xiandavis@yahoo.com>
2019-01-29 14:02:46 -0800

    collection views or grid layouts

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Christian Davis on 1/29/19.
+//  Copyright © 2019 xianapps. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
